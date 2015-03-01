dd0964df7a13888ac02f003a2f7bed44db54df74
Ron <mail@killerpixel.org>
2015-03-01 13:11:17 +0100

    module image

--- build/modules/image.php
+++ build/modules/image.php
@@ -1,5 +1,25 @@
 <?php
 
-echo 'this is a image module';
+/*handle file contents*/
+foreach ($files as $file) {
+
+	/*skip .ds store*/
+	if(substr($file,0,1) != '.'){
+
+		/*get filetype*/
+		$type = pathinfo($file, PATHINFO_EXTENSION);
+		if ($type == 'jpg'){
+
+			/*get file data*/
+			$filename = $content.'/'.$file;
+
+			/*print*/
+			echo '<img src="'.$filename.'" />';
+
+		}
+
+	}
+
+}
 
 ?>
