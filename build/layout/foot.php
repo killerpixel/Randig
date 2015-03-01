a36eb54dd2c7b9e87a608de237e161b389327a83
Ron <mail@killerpixel.org>
2015-03-01 14:57:57 +0100

    scripts added smoothscroll

--- build/layout/foot.php
+++ build/layout/foot.php
@@ -1,6 +1,9 @@
 
+	<!-- smooth scroll -->
+	<script type="text/javascript" src="build/scripts/smoothscroll/smoothscroll.js"></script>
+
 	<!-- accordion -->
-	<!--<script type="text/javascript" src="build/scripts/accordion/torpedo.accordion.js"></script>-->
+	<!--<script type="text/javascript" src="build/scripts/accordion/accordion.js"></script>-->
 
 </body>
 </html>
