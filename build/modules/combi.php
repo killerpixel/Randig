bf81139d7097baee440483434a33a7bf7e51ea00
Ron <mail@killerpixel.org>
2015-03-01 13:28:13 +0100

    module combi

--- build/modules/combi.php
+++ build/modules/combi.php
@@ -1,5 +1,41 @@
 <?php
 
-echo 'this is a combi module';
+/*handle file contents*/
+foreach ($files as $file) {
+
+	/*skip .ds store*/
+	if(substr($file,0,1) != '.'){
+
+		/*get filetype*/
+		$type = pathinfo($file, PATHINFO_EXTENSION);
+
+		/*handle image*/
+		if ($type == 'jpg'){
+
+			/*get file data*/
+			$filename = $content.'/'.$file;
+
+			/*print*/
+			echo '<img src="'.$filename.'" />';
+
+		}
+
+		/*handle text*/
+		if ($type == 'txt'){
+
+			/*get text data*/
+			$filename = $content.'/'.$file;
+			$file_open = fopen($filename, 'r');
+			$text = fread($file_open,filesize($filename));
+			fclose($file_open);
+
+			/*print*/
+			echo $text;
+
+		}
+
+	}
+
+}
 
 ?>
