1f4700184c3ebcdd2143b0920097f6d31b1315dd
Ron <mail@killerpixel.org>
2015-03-13 19:46:15 +0100

    module youtube embed

--- build/modules/youtube.php
+++ build/modules/youtube.php
@@ -4,8 +4,33 @@
 foreach ($files as $file) {
 
 	/*get only txt*/
+	$type = pathinfo($file, PATHINFO_EXTENSION);
+	if ($type == 'txt'){
 
-	/*get code from txt*/
+		/*get txt data*/
+		$filename = $content.'/'.$file;
+		$file_open = fopen($filename, 'r');
+		$input = fread($file_open,filesize($filename));
+		fclose($file_open);
+
+		/*handle code input*/
+		if(strpos($input, '?v=') !== false){
+			/*full url delivered*/
+			$code = substr(strrchr($input, "="), 1);
+		}else{
+			/*video code delivered*/
+			$code = $input;
+		}
+
+		/*print*/
+		echo '
+			<div class="video">
+				<img class="spacer" src="'.$content.'/'.'ratio.png" />
+				<iframe width="1280" height="720" src="//www.youtube.com/embed/'.$code.'?rel=0" frameborder="0" allowfullscreen></iframe>
+			</div>
+		';
+
+	}
 
 }
 
